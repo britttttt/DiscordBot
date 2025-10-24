e5650d2907a16f0a05df242e104845abaa40518a
Brit Beaty <britneybeaty@gmail.com>
2025-10-23 20:40:50 -0500

    troubleshooting in main.py

--- main.py
+++ main.py
@@ -76,10 +76,10 @@ async def valkyrie_error(ctx, error):
         await ctx.send("You do not have permissions to do that!")
     
 async def main():
-        await bot.load_extension("cogs.dadjoke")
-        await bot.load_extension("cogs.twitch")
-        await bot.load_extension("cogs.dice")
-        await bot.load_extension("cogs.tictactoe")
+        # await bot.load_extension("cogs.dadjoke")
+        # await bot.load_extension("cogs.twitch")
+        # await bot.load_extension("cogs.dice")
+        # await bot.load_extension("cogs.tictactoe")
         # await bot.load_extension("cogs.text_commands")
         
         await bot.start(token) 
